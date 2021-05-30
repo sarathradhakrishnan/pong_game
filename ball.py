27069486ea79eb42d1aa2cd1ec419352f7b3a0d0
sarathradhakrishnan <sarathrk1@outlook.com>
2021-05-30 09:28:04 +0530

    Added ball.

---
+++ ball.py
@@ -0,0 +1,14 @@
+from turtle import Turtle
+
+
+class Ball(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.color("white")
+        self.shape("circle")
+        self.penup()
+
+    def move(self):
+        new_x = self.xcor() + 1
+        new_y = self.ycor() + 1
+        self.goto(new_x, new_y)
